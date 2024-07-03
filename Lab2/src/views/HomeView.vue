f749c32265d261cfd233cede0307dd0efd36bd46
Bambam <bambam.pataranan@gmail.com>
2024-07-04 01:28:46 +0700

    Add title(Q9)

--- Lab2/src/views/HomeView.vue
+++ Lab2/src/views/HomeView.vue
@@ -41,6 +41,8 @@
 </script>
 
 <template>
+  <h1>Events For Good</h1>
+  <!-- new element -->
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>

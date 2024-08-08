5b9683d572f9021d937648def6bbb4fac7800067
Bambam <bambam.pataranan@gmail.com>
2024-08-09 02:24:30 +0700

    Use tailwind instead CSS (Lab5 Q5)

--- Lab2/src/components/EventCard.vue
+++ Lab2/src/components/EventCard.vue
@@ -19,7 +19,7 @@ defineProps<{
 
 <template>
   <RouterLink class="event-link" :to="{ name: 'event-detail-view' ,params: { id: event.id }}">
-      <div class="event-card hover:scale-101 hover:shadow-sp">
+      <div class="cursor-pointer border border-gray-600 p-4 w-64 mb-6 hover:scale-101 hover:shadow-sp">
         <h2>{{ event.title }}</h2>
         <span>@{{ event.time }} on {{ event.date }}</span>
       </div>
@@ -27,13 +27,6 @@ defineProps<{
 </template>
 
 <style scoped>
-.event-card {
-  padding: 20px;
-  width: 250px;
-  cursor: pointer;
-  border: 1px solid #39495c;
-  margin-bottom: 18px;
-}
 
 .event-link {
   text-decoration: none;

36e66397c9131cadd3d079ac1c19706d8578fcc0
Bambam <bambam.pataranan@gmail.com>
2024-08-09 01:25:43 +0700

    Add register function (Lab3 Q4)

--- Lab2/src/views/event/RegisterView.vue
+++ Lab2/src/views/event/RegisterView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { toRefs , defineProps} from 'vue'
 import { type Event } from '@/types'
+import { useRouter } from 'vue-router';
 
 const props = defineProps<{
   event: Event
@@ -8,8 +9,13 @@ const props = defineProps<{
 }>()
 //eslint-disable-next-line @typescript-eslint/no-unused-vars
 const { event } = toRefs(props)
+const router = useRouter()
+const register = () => {
+    router.push({ name: 'event-detail-view'})
+}
 </script>
 
 <template>
     <p>Register event here</p>
+    <button @click="register">Register Me!</button>
 </template>

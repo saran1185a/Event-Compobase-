b3b08ac12a9f67f887f3cd686cdefb5d11c364c1
Bambam <bambam.pataranan@gmail.com>
2024-08-09 02:32:15 +0700

    Animation css defined globally (Lab5 Q10)

--- Lab2/src/App.vue
+++ Lab2/src/App.vue
@@ -10,7 +10,7 @@ const { message } = storeToRefs(store)
 <template>
   <div class="text-center font-sans text-gray-700 antialias">
     <header>
-      <div id="flashMessage" v-if="message">
+      <div id="flashMessage" class="animate-fade" v-if="message">
         <h4>{{ message }}</h4>
       </div>
       <div class="wrapper">
@@ -32,16 +32,4 @@ h2 {
   font-size: 20px;
 }
 
-@keyframes yellofade {
-  from {
-    background-color: yellow;
-  }
-  to {
-    background-color: transparent;
-  }
-}
-
-#flashMessage {
-  animation: yellofade 3s ease-in-out;
-}
 </style>
